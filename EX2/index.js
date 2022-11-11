b49cacd41f385b7ac89c14777eece87bd4eb9667
Alexander <kerrymonich@yandex.ru>
2022-11-11 17:00:41 +0300

    EX3 complete EX2 homework FINISH

--- EX2/index.js
+++ EX2/index.js
@@ -31,3 +31,56 @@ while (startCount>finishCount) {
     dayCount++;
 }
 console.log(`На ${dayCount} день машин доступных к покупке станет меньше М`);
+
+//Проанализировать временной промежуток начиная с 1800 и до 2020 года найти и вывести в консоль
+//Год первого полета человека в космос и количество итераций которое потребовалось для поиска
+//Количество вискосных годов принадлежащих данному отрезку и количество итераций которое потребовалось для поиска
+
+let startDate = 1800;
+
+let finishDate = 2020;
+
+const firstCosmoFlight = 1961;
+
+let m = 0;
+
+for (m; startDate<firstCosmoFlight; m++) {
+    startDate++;
+}
+
+console.log (`Год полета первого человека в космос ${firstCosmoFlight}, для поиска потребовалось ${m} итераций`)
+
+let newDate = 1800;
+
+let k = 0;
+
+let d = 0;
+
+while (newDate<=finishDate) {
+    if (((newDate % 4 === 0) && (newDate % 100 !==0)) || (newDate % 400 === 0)) {
+        d++;
+        newDate++;
+    }else {
+        newDate++;
+    }
+}
+console.log(`Количество високосных годов в промежутке ${d}`);
+
+
+//Второй вариант решения
+let testStartDate = 1800;
+
+let testFinishDate = 2020;
+
+let p = 0;
+
+while (testStartDate<=testFinishDate) {
+    let myYear = testStartDate;
+    let realDate = new  Date (myYear, 1, 29).getDate ();
+    if (realDate == 29) {
+        p++;
+        testStartDate++;
+    }else {
+        testStartDate++;}
+}
+console.log(`Количество високосных годов в промежутке ${p}`);

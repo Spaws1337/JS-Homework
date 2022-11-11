c84327d472705706c3c493be50dfbb73f8e38154
Alexander <kerrymonich@yandex.ru>
2022-11-11 15:59:12 +0300

    EX1 Complete

---
+++ EX2/index.js
@@ -0,0 +1,12 @@
+// Вывести в консоль заданную строку N раз
+
+const messageString = ("Hello world");
+
+let stringCount = 21;
+
+let i = 0;
+
+while (i < stringCount) {
+    i++;
+    console.log (messageString);
+}
